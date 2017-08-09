fd0e20bb0fc3402ba3da26ed8ee1d1425380efea
Aleksander Heintz <alxandr@alxandr.me>
2017-08-09 08:11:51 +0200

    Test on multiple CLRs (#3)

--- src/YoloDev.PartialJson/IPartialProxy.cs
+++ src/YoloDev.PartialJson/IPartialProxy.cs
@@ -1,12 +1,16 @@
-﻿namespace YoloDev.PartialJson
+﻿using Newtonsoft.Json;
+
+namespace YoloDev.PartialJson
 {
   public interface IPartialProxy
   {
+    [JsonIgnore]
     IPartial Partial { get; }
   }
 
   public interface IPartialProxy<T> : IPartialProxy
   {
+    [JsonIgnore]
     new IPartial<T> Partial { get; }
   }
 }
